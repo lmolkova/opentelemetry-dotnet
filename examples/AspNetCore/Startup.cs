62953eee7667c1027488e89a794a3493d68ce925
Liudmila <lmolkova@microsoft.com>
2020-08-17 09:45:29 -0700

    hack attributes and tracestate back-propagation

--- examples/AspNetCore/Startup.cs
+++ examples/AspNetCore/Startup.cs
@@ -80,7 +80,7 @@ namespace Examples.AspNetCore
                 default:
                     services.AddOpenTelemetryTracing((builder) => builder
                         .AddAspNetCoreInstrumentation()
-                        .SetSampler(new ExternalPrioritySampler(0.1))
+                        .SetSampler(new ExternalPrioritySampler(0.7))
                         .AddHttpClientInstrumentation()
                         .AddConsoleExporter());
                     break;
