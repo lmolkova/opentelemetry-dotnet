aa1f1cb5197ec709513a2ee5b3b9e9ac5de2d20d
Liudmila <lmolkova@microsoft.com>
2020-09-02 12:47:11 -0700

    move generator to new file

--- src/OpenTelemetry/Trace/ExternalScoreSampler.cs
+++ src/OpenTelemetry/Trace/ExternalScoreSampler.cs
@@ -21,44 +21,6 @@ using System.Linq;
 namespace OpenTelemetry.Trace
 {
 
-    public abstract class SamplingScoreGenerator
-    {
-        public abstract double GenerateScore(in SamplingParameters parameters);
-    }
-
-    public class TraceIdRatioGenerator : SamplingScoreGenerator
-    {
-        public override double GenerateScore(in SamplingParameters samplingParameters)
-        {
-            Span<byte> traceIdBytes = stackalloc byte[16];
-            samplingParameters.TraceId.CopyTo(traceIdBytes);
-
-            return Math.Abs(this.GetLowerLong(traceIdBytes)) / (float)long.MaxValue;
-        }
-
-        private long GetLowerLong(ReadOnlySpan<byte> bytes)
-        {
-            long result = 0;
-            for (var i = 0; i < 8; i++)
-            {
-                result <<= 8;
-#pragma warning disable CS0675 // Bitwise-or operator used on a sign-extended operand
-                result |= bytes[i] & 0xff;
-#pragma warning restore CS0675 // Bitwise-or operator used on a sign-extended operand
-            }
-
-            return result;
-        }
-    }
-
-    public class RandomGenerator : SamplingScoreGenerator
-    {
-        public override double GenerateScore(in SamplingParameters parameters)
-        {
-            return new Random().NextDouble();
-        }
-    }
-
     public class ExternalScoreSampler : Sampler
     {
         private const string ScoreFlagName = "sampling.score";
---
+++ src/OpenTelemetry/Trace/SamplingScoreGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTelemetry.Trace
+{
+    public abstract class SamplingScoreGenerator
+    {
+        public abstract double GenerateScore(in SamplingParameters parameters);
+    }
+
+    public class TraceIdRatioGenerator : SamplingScoreGenerator
+    {
+        public override double GenerateScore(in SamplingParameters samplingParameters)
+        {
+            Span<byte> traceIdBytes = stackalloc byte[16];
+            samplingParameters.TraceId.CopyTo(traceIdBytes);
+
+            return Math.Abs(this.GetLowerLong(traceIdBytes)) / (float)long.MaxValue;
+        }
+
+        private long GetLowerLong(ReadOnlySpan<byte> bytes)
+        {
+            long result = 0;
+            for (var i = 0; i < 8; i++)
+            {
+                result <<= 8;
+#pragma warning disable CS0675 // Bitwise-or operator used on a sign-extended operand
+                result |= bytes[i] & 0xff;
+#pragma warning restore CS0675 // Bitwise-or operator used on a sign-extended operand
+            }
+
+            return result;
+        }
+    }
+
+    public class RandomGenerator : SamplingScoreGenerator
+    {
+        public override double GenerateScore(in SamplingParameters parameters)
+        {
+            return new Random().NextDouble();
+        }
+    }
+}
